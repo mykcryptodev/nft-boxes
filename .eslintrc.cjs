4ce8dda4bb749b79a5726600e6b22371811b9e0a
Mike Solomon <mike.solomon@coinbase.com>
2024-12-01 15:28:24 -0500

    allow ts ignore

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,6 +18,7 @@ const config = {
   "rules": {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
